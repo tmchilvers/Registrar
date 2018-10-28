4c6e670ce52ed83944cfe15d68bfe2a5c6e6417e
Matthew Parnham <parnham@chapman.edu>
2018-10-27 21:27:18 -0700

    Started converting GenQueue into a template.

--- GenQueue.h
+++ GenQueue.h
@@ -1,13 +1,17 @@
+#ifndef GENQUEUE_H
+#define GENQUEUE_H
+
 #include <iostream>
 
 using namespace std;
 
+template <class T>
 class GenQueue {
 public:
   GenQueue();
   GenQueue(int maxSize);
   ~GenQueue();
-  void insert(char data);
+  void insert(const T& data);
   char remove();
   char peek();
 
@@ -19,5 +23,46 @@ private:
   int rear;
   int numElements;
   int size;
-  char *myQueue;
+  T *myQueue;
 };
+
+template <class T>
+GenQueue<T>::GenQueue() {
+  front = NULL;
+  rear = NULL;
+  numElements = 0;
+  size = 100;
+  myQueue = new DoublyLinkedList();
+}
+
+template <class T>
+GenQueue<T>::~GenQueue() {
+
+}
+
+template <class T>
+void GenQueue<T>::insert(char data) {
+  myQueue.insertBack(data);
+}
+
+template <class T>
+char GenQueue<T>::remove() {
+  return myQueue.removeFront();
+}
+
+template <class T>
+char GenQueue<T>::peek() {
+  return myQueue.front;
+}
+
+template <class T>
+bool GenQueue<T>::isFull() {
+  //dynamic size.. will never be full
+}
+
+template <class T>
+bool GenQueue<T>::isEmpty() {
+  return myQueue == NULL;
+}
+
+#endif
