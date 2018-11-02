13c826427e439ea9b51dec58783c3ac9335ad26c
Tristan Chilvers <chilvers@chapman.edu>
2018-11-01 17:36:45 -0700

    what the ****

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include "GenQueue.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  GenQueue<int> gen;
+  gen.insert(new int(5));
+  gen.insert(new int(6));
+  gen.insert(new int(7));
+  gen.insert(new int(8));
+
+  cout << gen.getSize() << endl;
+
+  cout << *gen.remove() << endl;
+  cout << *gen.peek() << endl;
+  cout << gen.getSize() << endl;
+
+
+  return 0;
+}
